d94e78b81629bcfa7b9b8a5d96178b3456b63e2e
Robert Franklin <robertlsfranklin@gmail.com>
2022-12-15 14:58:58 +0800

    Created database in PostGres using ERD, created Jupyter Notebook and started coding

---
+++ Project2-ERD.sql
@@ -0,0 +1,37 @@
+﻿
+CREATE TABLE "application_record" (
+    "ID" int   NOT NULL,
+    "CODE_GENDER" text   NOT NULL,
+    "FLAG_OWN_CAR" text   NOT NULL,
+    "FLAG_OWN_REALTY" text   NOT NULL,
+    "CNT_CHILDREN" int   NOT NULL,
+    "AMT_INCOME_TOTAL" int   NOT NULL,
+    "NAME_INCOME_TYPE" text   NOT NULL,
+    "NAME_EDUCATION_TYPE" text   NOT NULL,
+    "NAME_FAMILY_STATUS" text   NOT NULL,
+    "NAME_HOUSING_TYPE" text   NOT NULL,
+    "DAYS_BIRTH" int   NOT NULL,
+    "DAYS_EMPLOYED" int   NOT NULL,
+    "FLAG_MOBILE" int   NOT NULL,
+    "FLAG_WORK_PHONE" int   NOT NULL,
+    "FLAG_PHONE" int   NOT NULL,
+    "FLAG_EMAIL" int   NOT NULL,
+    "OCCUPATION_TYPE" text   NOT NULL,
+    "CNT_FAM_MEMBERS" int   NOT NULL,
+    CONSTRAINT "pk_application_record" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE TABLE "credit_record" (
+    "ID" int   NOT NULL,
+    "MONTHS_BALANCE" INT   NOT NULL,
+    "STATUS" TEXT   NOT NULL,
+    CONSTRAINT "pk_credit_record" PRIMARY KEY (
+        "ID"
+     )
+);
+
+ALTER TABLE "application_record" ADD CONSTRAINT "fk_application_record_ID" FOREIGN KEY("ID")
+REFERENCES "credit_record" ("ID");
+
--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # Proyect2-DataAnalitics-Bootcamp
 
 ReadMe
+
+At least two data sources
+-	Kaggle
+-	Data.world
+-	Data.gov.au
+-	Google data set search
+Perform ETL (extract, transform, load)
+-	Datasets used and their sources
+-	Type of data wrangling performed
+    o	Data cleaning
+    o	Joining
+    o	Filtering
+    o	Aggregating
+-	Schemata used in the final production database
+Steps
+-	Research questions.
+-	Do relationship diagram (ERD).
+-	SQL (Postgres) or no SQL (MongoDB)?
+-	Transform into pandas dataframe
+-	Clean data
+-	Load it into database
+Project Proposal
+Report (word doc)
+Executive Summary, 1.5 spacing, page numbers
+1.	Extract
+2.	Transform
+    -	What transform was required?
+3.	Load
+
+GitHub repository
+-	Markup feature Jupyter Notebooks
+-	Readme
+-	gitignore
